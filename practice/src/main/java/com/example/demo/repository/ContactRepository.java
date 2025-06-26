30f5adfe8d3c9c943896078343cafd575c44deb4
windmikkun <windmikkun@gmail.com>
2025-06-26 23:44:14 +0900

    サービスクラスの実装

--- practice/src/main/java/com/example/demo/repository/ContactRepository.java
+++ practice/src/main/java/com/example/demo/repository/ContactRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.demo.entity.Contact;
 
 public interface ContactRepository extends JpaRepository<Contact, Long> {
-
+	
 }
